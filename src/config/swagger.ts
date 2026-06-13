9fd18bc56b73d9a59ae379ef35807f7c49e869a1
LisNat <rivera.liseth@correounivalle.edu.co>
2026-06-12 20:11:38 -0500

    fix(swagger): fix path resolution for windows compatibility

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -22,8 +22,8 @@ const options: swaggerJSDoc.Options = {
         ],
     },
     apis: [
-        path.join(__dirname, '../routes/*.ts'),
-        path.join(__dirname, '../routes/*.js'),
+        './src/routes/*.ts',
+        './src/routes/*.js',
     ],
 };
 
